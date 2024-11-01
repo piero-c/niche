2a081d8edceebf38c2f079581eed6c2591ec8864
Piero C <Camposeopiero@gmail.com>
2024-10-31 21:02:43 -0400

    Update files to reflect folder name change

--- scripts/db/config_loader.py
+++ scripts/db/config_loader.py
@@ -3,12 +3,12 @@ import os
 from dotenv import load_dotenv
 from pathlib import Path
 
-load_dotenv()
+load_dotenv(Path('../db/.env'))
 
 def load_config():
     """Load config based on env
     """
-    env = os.getenv('ENV', 'dev')
+    env = os.getenv('NODE_ENV')
     with open(Path('scripts/db/config.json')) as config_file:
         config = json.load(config_file)
     return(config.get(env))
